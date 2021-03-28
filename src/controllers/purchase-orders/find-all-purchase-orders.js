66ca0363a2b6bb92ed06b7cfdd1c0a3ef2e27893
gustavosimil <gustavo.simil@vaees.com>
2021-03-28 11:29:26 -0300

    feat: creation of the purchase order controller

---
+++ src/controllers/purchase-orders/find-all-purchase-orders.js
@@ -0,0 +1,16 @@
+const { serverError, success } = require('../../utils/http/http-helper');
+
+module.exports = class FindAllPurchaseOrdersController {
+    constructor(repository) {
+        this.repository = repository;
+    }
+
+    async handle() {
+        try {
+            const purchaseOrders = await this.repository.findAll();
+            return success({ purchaseOrders });
+        } catch (error) {
+            return serverError(error);
+        }
+    }
+};
